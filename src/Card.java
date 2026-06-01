f6bf3ca9669da794ba5a936c540d4c095857935a
JUSTICE WORMENOR <justicewormenor3@gmail.com>
2026-06-01 09:12:48 -0500

    Some small additions

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,7 @@
-public class Card {
+import java.util.ArrayList;
+
+public abstract class Card {
+
+    
+
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
+// Represents a player in the game.
+// Stores the player's name, hand and number of points.
+
 public class Player {
+
+    private String name;
+    private ArrayList<Card> hand;
+    private int numPoints;
+
+
+    // Creates a player with the given name.
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<Card>();
+        this.numPoints = 0;
+    }
+
+
 }
