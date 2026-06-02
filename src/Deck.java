0ae946b21ee52d862b92b3d09b5e8bd45d76e04c
ETHAN SAGUID <ethandwaynes@live.wsd1.org>
2026-06-02 11:14:01 -0500

    Added Deck Class Overview

--- src/Deck.java
+++ src/Deck.java
@@ -4,6 +4,26 @@ public class Deck {
 
     private ArrayList<Card> cards;
 
-
+    /*
+     * Deck Class
+     * Responsibilities:
+     * - Store all Card objects in a collection (ArrayList<Card>)
+     * - Create a standard 52-card deck using all suits and ranks
+     * - Shuffle the deck before a game starts
+     * - Deal cards to players and hands
+     * - Track how many cards remain in the deck
+     * - Reset/rebuild the deck when starting a new game
+     * Planned Methods:
+     * - Deck()               // Constructor
+     * - buildDeck()          // Creates all 52 cards
+     * - shuffle()            // Randomizes card order
+     * - dealCard()           // Returns and removes the top card
+     * - cardsRemaining()     // Returns number of cards left
+     * - isEmpty()            // Checks if deck is empty
+     * - reset()              // Rebuilds and shuffles deck
+     * Note:
+     * Waiting for Card class implementation before creating
+     * the deck generation logic.
+     */
 
 }
