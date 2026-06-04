6fe6177c4b438ab7eaf53a5a03fc42f7bac74e73
MARK JAMES GUEMO <markjamesg@live.wsd1.org>
2026-06-04 11:24:21 -0500

    randomly deleted my card class

--- src/Card.java
+++ src/Card.java
@@ -2,10 +2,28 @@ public class Card {
 
     private String suit;
     private String rank;
+    private int value;
 
     public Card(String suit, String rank) {
         this.suit = suit;
         this.rank = rank;
+
+
+        switch (rank) {
+            case "Ace":
+                value = 1;
+                break;
+
+            case "King":
+            case "Queen":
+            case "Jack":
+                value = 10;
+                break;
+
+            default:
+                value = Integer.parseInt(rank);
+                break;
+        }
     }
 
     // Added getters for encapsulation: allows read-only access to suit, rank, and value.
