b6be96e7e1c561c2be57f6b28b3dccc45b49edfd
MARK JAMES GUEMO <markjamesg@live.wsd1.org>
2026-06-09 10:12:47 -0500

    starting Game class

--- src/CasinoGame.java
+++ src/CasinoGame.java
@@ -1,4 +1,18 @@
 public abstract class CasinoGame {
+    String options = """
+            Games in the Casino:
+            Press B for Blackjack
+            Press W for War
+            Press P for Poker
+            Press E exit
+            """;
 
+    public void gameRun(){
+        while(true){
+            System.out.println(options);
+            Input.getUserString("Please select your choice: ");
 
+            
+        }
+    }
 }
